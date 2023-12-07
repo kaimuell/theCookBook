8b7be353701d72632485754db4117ead3f6fc817
kaimueller <me@kaimueller-code.com>
2023-12-07 12:39:35 +0100

    draft of entry page, stable difusion logo

---
+++ frontend-angular/src/app/recipe-creation-formular/recipe-creation-formular.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-recipe-creation-formular',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './recipe-creation-formular.component.html',
+  styleUrl: './recipe-creation-formular.component.css'
+})
+export class RecipeCreationFormularComponent {
+
+}

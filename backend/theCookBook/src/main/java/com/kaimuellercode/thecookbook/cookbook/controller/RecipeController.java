47a7fd7d93fa1f20180776942a6ed135270f72b8
kaimueller <me@kaimueller-code.com>
2023-12-08 13:24:27 +0100

    added getRecipeWithId to RecipeController

--- backend/theCookBook/src/main/java/com/kaimuellercode/thecookbook/cookbook/controller/RecipeController.java
+++ backend/theCookBook/src/main/java/com/kaimuellercode/thecookbook/cookbook/controller/RecipeController.java
@@ -5,6 +5,8 @@ import com.kaimuellercode.thecookbook.cookbook.core.Recipe;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 
 @RestController
@@ -33,5 +35,12 @@ public class RecipeController {
     @GetMapping(path = "cookable_with_ingredients")
     public List<Recipe> getRecipesCookableWithIngredients(@RequestParam List<String> ingredientNames){
         return cookBookService.getRecipesBookableWithIngredients(ingredientNames);
+    }
+
+    @GetMapping(path="withId")
+    public Recipe getRecipeWithId(@RequestParam long id){
+        Optional<Recipe> r =  cookBookService.getRecipeByID(id);
+        if (r.isEmpty()) throw new NoSuchElementException();
+        return r.get();
     }
 }
